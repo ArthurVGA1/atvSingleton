afc693faaa51eb0c291c40e8f08594d2389fea20
Arthur Verissimo <125267926+ArthurVGA1@users.noreply.github.com>
2024-09-26 17:06:11 -0300

    Atividade concluída

---
+++ Program.cs
@@ -0,0 +1,91 @@
+﻿public class Caldeira
+{
+    private static Caldeira _instancia;
+    public bool vazia { get; private set; } = true;
+    public bool fervida { get; private set; } = false;
+
+    private Caldeira() { } 
+
+    public static Caldeira GetInstancia()
+    {
+        if (_instancia == null)
+        {
+            _instancia = new Caldeira();
+        }
+        return _instancia;
+    }
+
+    public void encher()
+    {
+        if (vazia)
+        {
+            vazia = false;
+            fervida = false;
+            Console.WriteLine("Caldeira cheia");
+        }
+        else
+        {
+            Console.WriteLine("Erro: Caldeira já está cheia.");
+        }
+    }
+
+    public void ferver()
+    {
+        if (!vazia && !fervida)
+        {
+            fervida = true;
+            Console.WriteLine("A caldeira está fervendo");
+        }
+        else if (vazia)
+        {
+            Console.WriteLine("Erro: A caldeira está vazia.");
+        }
+        else
+        {
+            Console.WriteLine("Erro: A caldeira já está fervida.");
+        }
+    }
+
+    public void drenar()
+    {
+        if (fervida && !vazia)
+        {
+            fervida = false;
+            vazia = true;
+            Console.WriteLine("Caldeira drenada");
+        }
+        else if (vazia)
+        {
+            Console.WriteLine("Erro: A caldeira está vazia");
+        }
+        else
+        {
+            Console.WriteLine("Erro: A caldeira não está fervida");
+        }
+    }
+}
+
+public class FabricaDeChocolate
+{
+    public void TestarCaldeira()
+    {
+        var caldeira = Caldeira.GetInstancia();
+
+        caldeira.drenar();  
+        caldeira.encher();  
+        caldeira.ferver();  
+        caldeira.ferver();  
+        caldeira.drenar();  
+        caldeira.encher();  
+        caldeira.drenar();  
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        FabricaDeChocolate fabrica = new FabricaDeChocolate();
+        fabrica.TestarCaldeira();
+    }
+}
